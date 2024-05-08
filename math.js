d4924dc12ddf5266ef41fd1be81b7e7a4b906d3d
David Andersson <lejonmanen@gmail.com>
2024-05-08 11:13:35 +0200

    Test isZero

--- math.js
+++ math.js
@@ -6,5 +6,22 @@ function add(x, y) {
 	return x + y
 }
 
-export { add }
+/* 4 Utgå från följande kod, som testar en funktion manuellt. För att kunna testa funktionen måste vi veta vad den ska göra. Skriv acceptanskriterier, som tillsammans kan avgöra om funktionen uppfyller specen.*/
+// Om parametern value är noll ska funktionen returnera true
+// Om parametern value inte är noll ska funktionen returnera false
+
+function isZero(value) {
+	return value === 0
+}
+
+if( isZero(0) ) {
+    console.log('Talet är noll')
+}
+if( isZero(27) ) {
+    console.log('27 är inte noll')
+}
+
+
+
+export { add, isZero }
 
--- math.test.js
+++ math.test.js
@@ -1,5 +1,5 @@
-import { test, expect } from 'vitest'
-import { add } from './math.js'
+import { test, it, expect, describe } from 'vitest'
+import { add, isZero } from './math.js'
 
 function manualTest_add() {
 	// kör funktionen som ska testas med lämpliga värden
@@ -14,34 +14,50 @@ function manualTest_add() {
 }
 // manualTest_add()
 
-// test, expect, toBe
-test('can add 10 and 2', () => {
-	const expectedResult = 12
-	const actualResult = add(10, 2)
-	expect(actualResult).toBe(expectedResult)
-})
+describe('add(x, y)', () => {
+	test('can add 10 and 2', () => {
+		const expectedResult = 12
+		const actualResult = add(10, 2)
+		expect(actualResult).toBe(expectedResult)
+	})
 
-test('can add 20 and 0', () => {
-	const expectedResult = 20
-	const actualResult = add(20, 0)
-	expect(actualResult).toBe(expectedResult)
-})
-
-
-test('can add -10 and 5', () => {
-	const expectedResult = -5
-	const actualResult = add(-10, 5)
-	expect(actualResult).toBe(expectedResult)
-})
+	test('can add 20 and 0', () => {
+		const expectedResult = 20
+		const actualResult = add(20, 0)
+		expect(actualResult).toBe(expectedResult)
+	})
 
 
-test('returns NaN when x is not a number', () => {
-	const expected = NaN
-	const actual = add('20', 2)
-	expect(actual).toBe(expected)
+	test('can add -10 and 5', () => {
+		const expectedResult = -5
+		const actualResult = add(-10, 5)
+		expect(actualResult).toBe(expectedResult)
+	})
+
+
+	test('returns NaN when x is not a number', () => {
+		const expected = NaN
+		const actual = add('20', 2)
+		expect(actual).toBe(expected)
+	})
+	test('returns NaN when y is not a number', () => {
+		const expected = NaN
+		const actual = add(42, '101')
+		expect(actual).toBe(expected)
+	})
 })
-test('returns NaN when y is not a number', () => {
-	const expected = NaN
-	const actual = add(42, '101')
-	expect(actual).toBe(expected)
+
+
+describe('isZero(x)', () => {
+	it('returns true if the parameter is zero', () => {
+		expect( isZero(0) ).toBe( true )
+	})
+
+	it('returns false if the parameter is 27', () => {
+		expect( isZero(27) ).toBe( false )
+	})
+	it('returns false if the parameter is "blå"', () => {
+		expect( isZero('blå') ).toBe( false )
+	})
 })
+// it('returns false if the parameter is not zero')
