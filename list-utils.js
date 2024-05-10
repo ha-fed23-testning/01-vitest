5a51089bdc59c217837e77bd23b40355e6621b41
David Andersson <lejonmanen@gmail.com>
2024-05-10 15:17:47 +0200

    2.7 arrayMax

--- list-utils.js
+++ list-utils.js
@@ -1,9 +1,33 @@
+function isArray(x) {
+	if( x === null ) return false
+	return (typeof x) === 'object' && (typeof x.length) === 'number'
+}
+
 function arrayLength(array) {
-	if( (typeof array) === 'object' && (typeof array.length) === 'number' ) {
+	if( isArray(array) ) {
 		return array.length
 	} else {
 		return -1
 	}
 }
 
-export { arrayLength }
+function arrayMax(array) {
+	if( !isArray(array) ) {
+		throw new Error('parameter must be an array')
+	} else if( array.length === 0 ) {
+		return undefined
+	}
+	const numbers = array.filter(x => (typeof x) === 'number')
+	if( numbers.length === 0 ) {
+		return NaN
+	}
+	let maximum = numbers[0]
+	numbers.forEach(n => {
+		if( n > maximum ) {
+			maximum = n
+		}
+	})
+	return maximum
+}
+
+export { arrayLength, arrayMax, isArray }
--- list-utils.test.js
+++ list-utils.test.js
@@ -1,5 +1,16 @@
 import { describe, it, test, /* it och test är samma funktion */ expect } from 'vitest'
-import { arrayLength } from './list-utils.js'
+import { arrayLength, arrayMax, isArray } from './list-utils.js'
+
+describe('isArray(x)', () => {
+	it('returns true if parameter is an array', () => {
+		expect( isArray(['p', 'uwuww']) ).toBe(true)
+		expect( isArray([]) ).toBe(true)
+	})
+	it('returns false if parameter is not an array', () => {
+		expect( isArray(null) ).toBe(false)
+		expect( isArray(27) ).toBe(false)
+	})
+})
 
 describe('arrayLength(array)', () => {
 	// returnera längden på listan
@@ -15,6 +26,28 @@ describe('arrayLength(array)', () => {
 	})
 })
 
+describe('arrayMax(array)', () => {
+	// om inte array, throw Error
+	// om arrayen är tom, returnera undefined
+	// om arrayen har element, fast inga är tal, returnera NaN
+	// annars returnera största talet
+	it('throws an Error if parameter not an array', () => {
+		expect( () => arrayMax(null) ).toThrow()
+	})
+	it('returns undefined if the array is empty', () => {
+		expect( arrayMax([]) ).toBeUndefined()
+	})
+	it('returns NaN if array only has non-numbers', () => {
+		expect( arrayMax(['pele', false]) ).toBeNaN()
+		expect( arrayMax([null]) ).toBeNaN()
+		expect( arrayMax([true, false, undefined]) ).toBeNaN()
+	})
+	it('returns the maximum number of the array', () => {
+		expect( arrayMax([-10]) ).toBe(-10)
+		expect( arrayMax([-1, 20, -5, 15]) ).toBe(20)
+		expect( arrayMax([-10, -11, -12, -13]) ).toBe(-10)
+	})
+})
 /*
 6 En funktion som returnerar längden på en array. Om input inte är en array ska den returnera -1.
 
